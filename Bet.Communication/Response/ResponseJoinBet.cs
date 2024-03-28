2800ccbd90212459ec602072b5d9206d2f6a296a
vts1978 <vinicioussouza@gmail.com>
2024-03-27 22:01:58 -0300

    update: respondeJoinbet now have two more properties possible return and chosen team

--- Bet.Communication/Response/ResponseJoinBet.cs
+++ Bet.Communication/Response/ResponseJoinBet.cs
@@ -1,5 +1,9 @@
-﻿namespace Bet.Communication.Response;
+﻿using Bet.Domain.Entities;
+
+namespace Bet.Communication.Response;
 public class ResponseJoinBet
 {
     public double Value { get; set; }
+    public double PossibleReturn { get; set; }
+    public Team Chose { get; set; }
 }
