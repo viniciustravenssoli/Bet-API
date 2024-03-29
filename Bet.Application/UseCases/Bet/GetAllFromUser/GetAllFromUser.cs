321ed321584ecfdc09fdae592159e2a5559bf0af
vts1978 <vinicioussouza@gmail.com>
2024-03-29 19:36:46 -0300

    new: use case getall bets from user

---
+++ Bet.Application/UseCases/Bet/GetAllFromUser/GetAllFromUser.cs
@@ -0,0 +1,29 @@
+﻿
+using Bet.Application.Services.LoggedUser;
+using Bet.Communication.Response;
+using Bet.Domain.Repository.UserBet;
+
+namespace Bet.Application.UseCases.Bet.GetAllFromUser;
+public class GetAllFromUser : IGetAllFromUser
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IUserBetWriteOnlyRepository _userBetWriteOnlyRepository;
+
+    public GetAllFromUser(ILoggedUser loggedUser, IUserBetWriteOnlyRepository userBetWriteOnlyRepository)
+    {
+        _loggedUser = loggedUser;
+        _userBetWriteOnlyRepository = userBetWriteOnlyRepository;
+    }
+
+    public async Task<ResponseGetAllBets> Execute(int page, int PageSize)
+    {
+        var user = await _loggedUser.GetUser();
+
+        var betsFromUser = await _userBetWriteOnlyRepository.GetUserBetsWithPaginationAsync(user.Id, page, PageSize);
+
+        var response = new ResponseGetAllBets(betsFromUser);
+
+        return response;
+
+    }
+}
---
+++ Bet.Application/UseCases/Bet/GetAllFromUser/IGetAllFromUser.cs
@@ -0,0 +1,7 @@
+﻿using Bet.Communication.Response;
+
+namespace Bet.Application.UseCases.Bet.GetAllFromUser;
+public interface IGetAllFromUser
+{
+    Task<ResponseGetAllBets> Execute(int page, int PageSize);
+}
