dd9591147a012b5ed21d1b195839429ba6d33d1f
vts1978 <vinicioussouza@gmail.com>
2024-03-29 19:35:38 -0300

    new: response to new use case

---
+++ Bet.Communication/Response/ResponseGetAllBets.cs
@@ -0,0 +1,12 @@
+﻿using Bet.Domain.Entities;
+
+namespace Bet.Communication.Response;
+public class ResponseGetAllBets
+{
+    public List<UserBet> bets { get; set; }
+
+    public ResponseGetAllBets(List<UserBet> userBets)
+    {
+        bets = userBets;
+    }
+}
