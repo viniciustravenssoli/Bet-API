37261b2d16c3a80299b308ea18ba458244ecf836
vts1978 <vinicioussouza@gmail.com>
2024-04-16 14:41:27 -0300

    fix: Phone validator in change user info

--- src/Bet.Application/UseCases/User/ChangeUserData/ChangeUserDataValidator.cs
+++ src/Bet.Application/UseCases/User/ChangeUserData/ChangeUserDataValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Bet.Communication.Request;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace Bet.Application.UseCases.User.ChangeUserData;
 public class ChangeUserDataValidator : AbstractValidator<RequestChangeUserData>
@@ -13,7 +14,16 @@ public class ChangeUserDataValidator : AbstractValidator<RequestChangeUserData>
 
         When(c => !string.IsNullOrEmpty(c.NewPhone), () =>
         {
-            RuleFor(c => c.NewPhone).Matches("[0-9]{2} [1-9]{1} [0-9]{4}-[0-9]{4}").WithMessage("InvalidPhoneNumber");
+            RuleFor(c => c.NewPhone).Custom((telefone, context) =>
+            {
+                string patternPhone = "[0-9]{2} [1-9]{1} [0-9]{4}-[0-9]{4}";
+                var isMatch = Regex.IsMatch(telefone, patternPhone);
+
+                if (!isMatch)
+                {
+                    context.AddFailure(new FluentValidation.Results.ValidationFailure(nameof(telefone), "InvalidPhoneNumber"));
+                }
+            });
         });
 
         When(c => !string.IsNullOrEmpty(c.NewName), () =>
