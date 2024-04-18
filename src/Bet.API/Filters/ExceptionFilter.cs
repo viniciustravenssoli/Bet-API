58026fbadfd23723f9dd67ad61734d9544e776c2
vts1978 <vinicioussouza@gmail.com>
2024-04-17 21:25:34 -0300

    adding ILogger to expections

--- src/Bet.API/Filters/ExceptionFilter.cs
+++ src/Bet.API/Filters/ExceptionFilter.cs
@@ -8,8 +8,16 @@ namespace Bet.API.Filters;
 
 public class ExceptionFilter : IExceptionFilter
 {
+    private readonly ILogger<ExceptionFilter> _logger;
+
+    public ExceptionFilter(ILogger<ExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
     public void OnException(ExceptionContext context)
     {
+        _logger.LogError(context.Exception, "An error occurred.");
+
         if (context.Exception is BetException)
         {
             DealBetExceptions(context);
