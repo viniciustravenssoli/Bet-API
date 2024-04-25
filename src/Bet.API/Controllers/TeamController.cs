0784286a8e361340537202a467d3652a0c8db2cd
vts1978 <vinicioussouza@gmail.com>
2024-04-24 21:09:25 -0300

    feat: teamController

---
+++ src/Bet.API/Controllers/TeamController.cs
@@ -0,0 +1,19 @@
+﻿using Bet.Application.UseCases.Team.RegisterTeam;
+using Bet.Communication.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bet.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TeamController : BaseBetController
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> RegistrarTime(
+      [FromServices] IRegisterTeamUseCase useCase,
+      [FromBody] RequestRegisterTeam request)
+    {
+        await useCase.Execute(request);
+
+        return Created(string.Empty, "Criado com sucesso");
+    }
+}
