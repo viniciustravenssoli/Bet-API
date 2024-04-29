4f98da4f6b612d2cc3f19c70ad676d65f8416c72
vts1978 <vinicioussouza@gmail.com>
2024-04-29 17:13:32 -0300

    refact: ExceptionFilter

--- src/Bet.API/Filters/ExceptionFilter.cs
+++ src/Bet.API/Filters/ExceptionFilter.cs
@@ -4,84 +4,65 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Net;
 
-namespace Bet.API.Filters;
-
-public class ExceptionFilter : IExceptionFilter
+namespace Bet.API.Filters
 {
-    private readonly ILogger<ExceptionFilter> _logger;
-
-    public ExceptionFilter(ILogger<ExceptionFilter> logger)
+    public class ExceptionFilter : IExceptionFilter
     {
-        _logger = logger;
-    }
-    public void OnException(ExceptionContext context)
-    {
-        _logger.LogError(context.Exception, "An error occurred.");
+        private readonly ILogger<ExceptionFilter> _logger;
 
-        if (context.Exception is BetException)
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
         {
-            DealBetExceptions(context);
+            _logger = logger;
         }
-        else
+
+        public void OnException(ExceptionContext context)
         {
-            ThrowUnkownError(context);
-        }
-    }
+            _logger.LogError(context.Exception.Message, "An error occurred.");
 
-    private void DealBetExceptions(ExceptionContext context)
-    {
-        if (context.Exception is ValidationErrorException)
+            switch (context.Exception)
+            {
+                case BetException betException:
+                    DealBetException(context, betException);
+                    break;
+                default:
+                    ThrowUnknownError(context);
+                    break;
+            }
+        }
+
+        private void DealBetException(ExceptionContext context, BetException exception)
         {
-            DealValidationErrorsException(context);
+            var statusCode = HttpStatusCode.InternalServerError;
+            var errorMessage = "Unknown error occurred";
+
+            switch (exception)
+            {
+                case NotFoundException notFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    errorMessage = notFoundException.Message;
+                    break;
+                case ConflictException conflictException:
+                    statusCode = HttpStatusCode.Conflict;
+                    errorMessage = conflictException.Message;
+                    break;
+                case ValidationErrorException validationErrorException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    errorMessage = string.Join(", ", validationErrorException.ErrorMessages);
+                    break;
+                case LoginInvalidException loginInvalidException:
+                    statusCode = HttpStatusCode.Unauthorized;
+                    errorMessage = loginInvalidException.Message;
+                    break;
+            }
+
+            context.HttpContext.Response.StatusCode = (int)statusCode;
+            context.Result = new ObjectResult(new ErrorResponse(errorMessage));
         }
-        else if (context.Exception is LoginInvalidException)
+
+        private void ThrowUnknownError(ExceptionContext context)
         {
-            DealLoginException(context);
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new ObjectResult(new ErrorResponse("Unknown error occurred"));
         }
-        else if (context.Exception is NotFoundException)
-        {
-            DealNotFoundException(context);
-        }
-        else if(context.Exception is ConflictException) 
-        {
-            DealConfilictException(context);
-        }
-    }
-
-    private void DealNotFoundException(ExceptionContext context)
-    {
-        var notFound = context.Exception as NotFoundException;
-
-        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
-        context.Result = new ObjectResult(new ErrorResponse(notFound.Message));
-    }
-
-    private void DealConfilictException(ExceptionContext context)
-    {
-        var notFound = context.Exception as ConflictException;
-
-        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Conflict;
-        context.Result = new ObjectResult(new ErrorResponse(notFound.Message));
-    }
-
-    private void DealValidationErrorsException(ExceptionContext context)
-    {
-        var validationErrorException = context.Exception as ValidationErrorException;
-
-        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-        context.Result = new ObjectResult(new ErrorResponse(validationErrorException.ErrorMessages));
-    }
-    private void DealLoginException(ExceptionContext context)
-    {
-        var erroLogin = context.Exception as LoginInvalidException;
-
-        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-        context.Result = new ObjectResult(new ErrorResponse(erroLogin.Message));
-    }
-
-    private void ThrowUnkownError(ExceptionContext context)
-    {
-        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        context.Result = new ObjectResult(new ErrorResponse("Erro desconhecido"));
     }
 }
